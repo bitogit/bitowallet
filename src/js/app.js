30633f9e9df4d7c58a5905e5466106f96edbd198
Vincent Khougaz <vkhougaz@serverbox>
2017-02-21 12:38:24 -0800

    Adding new whitelabel (based on copay) and new addon (based off coloredcoins)

--- src/js/app.js
+++ src/js/app.js
@@ -26,4 +26,4 @@ angular.module('copayApp.filters', []);
 angular.module('copayApp.services', []);
 angular.module('copayApp.controllers', []);
 angular.module('copayApp.directives', []);
-angular.module('copayApp.addons', []);
+angular.module('copayApp.addons', ['copayAddon.retailBenefits']);
