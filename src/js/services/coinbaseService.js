f08c3069d7812069d9f516f5a0269cdb74d86c23
Vincent Khougaz <vincent@khougaz.com>
2017-05-29 13:08:39 -0700

    Reduced coinbase send limit to /week, http://wallet.bitovation.com/

--- src/js/services/coinbaseService.js
+++ src/js/services/coinbaseService.js
@@ -123,7 +123,7 @@ angular.module('copayApp.services').factory('coinbaseService', function($http, $
       credentials.REDIRECT_URI +
       '&state=SECURE_RANDOM&scope=' +
       credentials.SCOPE +
-      '&meta[send_limit_amount]=1000&meta[send_limit_currency]=USD&meta[send_limit_period]=day';
+      '&meta[send_limit_amount]=1&meta[send_limit_currency]=USD&meta[send_limit_period]=week';
   };
 
   root.getToken = function(code, cb) {
